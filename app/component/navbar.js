cba51ed915c555d4a24ca8085716fa865f264e5a
Ramey101 <aryav101@gmail.com>
2023-12-13 16:18:45 +0800

    each pages file content updated

--- app/component/navbar.js
+++ app/component/navbar.js
@@ -20,9 +20,9 @@ const { default: Link } = require("next/link")
 
 
 const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Web  3.0', 'Tech', 'Documentation'];
+const navItems = ['Home', 'Web 3.0', 'Tech', 'Documentation'];
 const hrefs = [
-    '/', 'about', 'web3', 'tech', 'Documentation'
+    '/', 'web3', 'tech', 'https://myid-1.gitbook.io/web3/'
 ];
 
 
@@ -40,10 +40,10 @@ function DrawerAppBar(props) {
                 MYID
             </Typography> */}
             <Link href="/">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 text-white transition-colors duration-300 group-hover:text-blue-500 cursor-pointer">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
-                    </svg>
-                </Link>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 text-white transition-colors duration-300 group-hover:text-blue-500 cursor-pointer">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
+                </svg>
+            </Link>
             <Divider />
             <List>
                 {navItems.map((item, index) => (
@@ -82,13 +82,13 @@ function DrawerAppBar(props) {
                     >
                         MYID
                     </Typography>
-                    
+
                     <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                         {navItems.map((item, index) => (
-                            <Button 
-                            key={item} 
-                            href={hrefs[index]} 
-                            sx={{ color: '#fff', background: 'transparent', fontWeight: 'bold', '&:hover': { color: '#87CEFA', background: 'transparent' } }}>
+                            <Button
+                                key={item}
+                                href={hrefs[index]}
+                                sx={{ color: '#fff', background: 'transparent', fontWeight: 'bold', '&:hover': { color: '#87CEFA', background: 'transparent' } }}>
                                 {item}
                             </Button>
                         ))}
